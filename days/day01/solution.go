9fcf067ef86087cdc04cc1e713f0ef50e278fa92
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-12-01 13:12:56 +0100

    feat: day01 solution

--- days/day01/solution.go
+++ days/day01/solution.go
@@ -1,19 +1,53 @@
 package main
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 func part1(input string) any {
+	dial := 50
+
+	passwordPointer := 0
+
 	for _, line := range strings.Split(input, "\n") {
-		_ = line
+		toDial, _ := strconv.Atoi(line[1:])
+		rotation := []rune(line[:1])[0]
+		switch rotation {
+		case 'L':
+			dial -= toDial
+		case 'R':
+			dial += toDial
+		}
+
+		dial = dial % 100
+
+		if dial == 0 {
+			passwordPointer++
+		}
 	}
 
-	return nil
+	return passwordPointer
 }
 
 func part2(input string) any {
+	dial := 50
+	passwordPointer := 0
+
 	for _, line := range strings.Split(input, "\n") {
-		_ = line
+		toDial, _ := strconv.Atoi(line[1:])
+		for range toDial {
+			switch line[0] {
+			case 'L':
+				dial--
+			case 'R':
+				dial++
+			}
+			if dial%100 == 0 {
+				passwordPointer++
+			}
+		}
 	}
 
-	return nil
+	return passwordPointer
 }
