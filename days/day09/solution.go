74dd4855af68dcefde6789cb1d1c07b1b0bc0baa
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-12-09 09:45:22 +0100

    feat: day09 part 2

--- days/day09/solution.go
+++ days/day09/solution.go
@@ -14,6 +14,20 @@ func abs(n int) int {
 	return n
 }
 
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func (t *Tile) Area(corner *Tile) int {
 	w := abs(t[0]-(*corner)[0]) + 1
 	h := abs(t[1]-(*corner)[1]) + 1
@@ -24,9 +38,11 @@ func part1(input string) any {
 	lines := strings.Split(input, "\n")
 	tiles := make([]*Tile, len(lines))
 	for i, line := range strings.Split(input, "\n") {
+		line = strings.TrimSpace(line)
+
 		ints := strings.Split(line, ",")
-		x1, _ := strconv.Atoi(ints[0])
-		x2, _ := strconv.Atoi(ints[1])
+		x1, _ := strconv.Atoi(strings.TrimSpace(ints[0]))
+		x2, _ := strconv.Atoi(strings.TrimSpace(ints[1]))
 		tiles[i] = &Tile{x1, x2}
 	}
 
@@ -42,13 +58,62 @@ func part1(input string) any {
 
 func part2(input string) any {
 	lines := strings.Split(input, "\n")
-	tiles := make([]*Tile, len(lines))
-	for i, line := range strings.Split(input, "\n") {
+
+	tiles := make([]Tile, len(lines))
+	for i, line := range lines {
+		line = strings.TrimSpace(line)
+
 		ints := strings.Split(line, ",")
-		x1, _ := strconv.Atoi(ints[0])
-		x2, _ := strconv.Atoi(ints[1])
-		tiles[i] = &Tile{x1, x2}
+		x1, _ := strconv.Atoi(strings.TrimSpace(ints[0]))
+		x2, _ := strconv.Atoi(strings.TrimSpace(ints[1]))
+		tiles[i] = Tile{x1, x2}
 	}
 
-	return nil
+	ans := 0
+	n := len(tiles)
+
+	for i := range n {
+		for j := i + 1; j < n; j++ {
+
+			a := tiles[i]
+			b := tiles[j]
+
+			w := abs(a[0]-b[0]) + 1
+			h := abs(a[1]-b[1]) + 1
+			area := w * h
+
+			if area <= ans {
+				continue
+			}
+
+			possible := true
+			xmin := min(a[0], b[0])
+			xmax := max(a[0], b[0])
+			ymin := min(a[1], b[1])
+			ymax := max(a[1], b[1])
+
+			for k := 0; k < len(tiles); k++ {
+				l := (k + 1) % n
+
+				x1, y1 := tiles[k][0], tiles[k][1]
+				x2, y2 := tiles[l][0], tiles[l][1]
+
+				outside := (x1 <= xmin && x2 <= xmin) || // fully left
+					(x1 >= xmax && x2 >= xmax) || // fully right
+					(y1 <= ymin && y2 <= ymin) || // fully below
+					(y1 >= ymax && y2 >= ymax) // fully above
+
+				if !outside {
+					possible = false
+					break
+				}
+			}
+
+			if possible {
+				ans = area
+			}
+		}
+	}
+
+	return ans
 }
